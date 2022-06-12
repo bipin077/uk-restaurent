860c49e8a5ef76d256692a57d65e0273f3057992
bipin077 <80197490+bipin077@users.noreply.github.com>
2022-06-12 18:47:37 +0530

    Update ItemStatic.jsx

--- src/Components/Pages/Home/ItemStatic.jsx
+++ src/Components/Pages/Home/ItemStatic.jsx
@@ -4,7 +4,7 @@ function ItemStatic(props) {
   return (
     <>
         <div className="image-item">
-            <img src={props.items.image} alt="sdfasdd" />
+            <img src={require(props.items.image)} alt="sdfasdd" />
             <h2>{props.items.title}</h2>
             <p>{props.items.subtitle}</p>
         </div>
